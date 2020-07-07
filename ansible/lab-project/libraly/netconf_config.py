bfb733846ff92618195ce6f192c71ed35bf9bad9
Ib Sangaré <sibrahima250@gmail.com>
2020-07-07 14:14:04 +0100

    netconf: role vrf

--- ansible/lab-project/libraly/netconf_config.py
+++ ansible/lab-project/libraly/netconf_config.py
@@ -198,6 +198,101 @@ def main():
         mutually_exclusive=[('xml', 'src')]
     )
 
+    if not HAS_NCCLIENT:
+        module.fail_json(msg='could not import the python library '
+                         'ncclient required by this module')
+
+    if (module.params['src']):
+        config_xml = str(module.params['src'])
+    elif module.params['xml']:
+        config_xml = str(module.params['xml'])
+    else:
+        module.fail_json(msg='Option src or xml must be provided')
+
+    try:
+        xml.dom.minidom.parseString(config_xml)
+
+    except Exception as e:
+        module.fail_json(msg='error parsing XML: %s' % to_native(e), exception=traceback.format_exc())
+
+    nckwargs = dict(
+        host=module.params['host'],
+        port=module.params['port'],
+        hostkey_verify=module.params['hostkey_verify'],
+        allow_agent=module.params['allow_agent'],
+        look_for_keys=module.params['look_for_keys'],
+        username=module.params['username'],
+        password=module.params['password'],
+        timeout=module.params['timeout'],
+    )
+
+    try:
+        m = ncclient.manager.connect(**nckwargs)
+    except ncclient.transport.errors.AuthenticationError:
+        module.fail_json(
+            msg='authentication failed while connecting to device'
+        )
+    except Exception as e:
+        module.fail_json(msg='error connecting to the device: %s' % to_native(e), exception=traceback.format_exc())
+
+    retkwargs = dict()
+    retkwargs['server_capabilities'] = list(m.server_capabilities)
+
+    if module.params['datastore'] == 'candidate':
+        if ':candidate' in m.server_capabilities:
+            datastore = 'candidate'
+        else:
+            m.close_session()
+            module.fail_json(
+                msg=':candidate is not supported by this netconf server'
+            )
+    elif module.params['datastore'] == 'running':
+        if ':writable-running' in m.server_capabilities:
+            datastore = 'running'
+        else:
+            m.close_session()
+            module.fail_json(
+                msg=':writable-running is not supported by this netconf server'
+            )
+    elif module.params['datastore'] == 'auto':
+        if ':candidate' in m.server_capabilities:
+            datastore = 'candidate'
+        elif ':writable-running' in m.server_capabilities:
+            datastore = 'running'
+        else:
+            m.close_session()
+            module.fail_json(
+                msg='neither :candidate nor :writable-running are supported by this netconf server'
+            )
+    else:
+        m.close_session()
+        module.fail_json(
+            msg=module.params['datastore'] + ' datastore is not supported by this ansible module'
+        )
+
+    if module.params['save']:
+        if ':startup' not in m.server_capabilities:
+            module.fail_json(
+                msg='cannot copy <running/> to <startup/>, while :startup is not supported'
+            )
+
+    try:
+        changed = netconf_edit_config(
+            m=m,
+            xml=config_xml,
+            commit=True,
+            retkwargs=retkwargs,
+            datastore=datastore,
+        )
+        if changed and module.params['save']:
+            m.copy_config(source="running", target="startup")
+    except Exception as e:
+        module.fail_json(msg='error editing configuration: %s' % to_native(e), exception=traceback.format_exc())
+    finally:
+        m.close_session()
+
+    module.exit_json(changed=changed, **retkwargs)
+
 
 if __name__ == '__main__':
     main()
