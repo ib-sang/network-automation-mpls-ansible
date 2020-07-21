1a65e8dade5454c5f81a80b0790767fe0910f98b
Ib Sangaré <sibrahima250@gmail.com>
2020-07-22 00:09:31 +0100

    django start with ansible

--- ansible/mpls_automation/mpls_monitor/views.py
+++ ansible/mpls_automation/mpls_monitor/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 import subprocess, json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 
 def index(request):
 	DeviceList = [('R1 - Cisco ios', 'R1'), ('R2 - Cisco ios', 'R2'),
 	 				('R3 - Junos srx', 'R3'), ('R4 - Cisco ios xr', 'R4'),
-	 				('R5 - Cisco ios', 'R5')]
+	 				]
 	return render(request, 'index.html', {'DeviceList': DeviceList})
 
 
 def ping_test(request):
-	device = request.POST.get('device_name')
+	device = request.POST.get('device-name') 
 	result = subprocess.Popen(['ansible-playbook', 'ping-playbook.yml', '--extra-vars', 'host='+device ], stdout=subprocess.PIPE)
 	output, err = result.communicate()
 	return JsonResponse(json.loads(output))
